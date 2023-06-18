019432087816b103b73605743222da77b314bcdd
José Miguel Horcas <horcas@uma.es>
2023-06-18 18:14:54 +0200

    Refactoring login and registration

--- app/blueprints/auth/__init__.py
+++ app/blueprints/auth/__init__.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 
 auth_bp = Blueprint('auth', 
                     __name__, 
-                    template_folder='templates')
+                    template_folder='templates',
+                    static_folder='static',
+                    static_url_path='/auth/static')
 
 from . import routes
--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -2,7 +2,6 @@ from flask import render_template, request, redirect, url_for, current_app, flas
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from . import auth_bp
-from .forms import LoginForm, SignupForm
 from .models import User, get_user
 
 
@@ -11,41 +10,41 @@ from .models import User, get_user
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('admin.index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = get_user(form.email.data)
-        if user is not None and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        remember_me = bool(request.form.get('rememberme', False))
+
+        user = get_user(email)
+        if user is not None and user.check_password(password):
+            login_user(user, remember=remember_me)
             next_page = request.args.get('next')
             if not next_page or url_parse(next_page).netloc != '':
                 next_page = url_for('admin.index')
             return redirect(next_page)
-    return render_template('auth/login.html', form=form)
+        else:
+            flash(f'Invalid email or password.', category='error')
+    return render_template('auth/login.html')
 
 
 @auth_bp.route("/signup/", methods=["GET", "POST"])
 @login_required
 def show_signup_form():
-    #if current_user.is_authenticated:
-    #    return redirect(url_for('table.index'))
-    form = SignupForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-        password = form.password.data
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
         # Create and save the user
-        user_id = len(User.objects) + 1
-        user = User(id=user_id, username=name, email=email)
+        if User.objects(email=email):
+            flash(f"There exists an admin with this email.", category='error')
+            return render_template('auth/register.html')
+        print(f'email: {email}')
+        print(f'password: {password}')
+        user = User(email=email)
         user.set_password(password)
         user.save()
-        flash(f'New admin successfully registered.')
-        # Maintain the user logged
-        #login_user(user, remember=True)
-        #next_page = request.args.get('next', None)
-        #if not next_page or url_parse(next_page).netloc != '':
-        #    next_page = url_for('table.index')
-        #return redirect(next_page)
-    return render_template("auth/register.html", form=form)
+        flash(f'New admin successfully registered.', category='info')
+    return render_template("auth/register.html")
 
 
 @auth_bp.route('/logout')
