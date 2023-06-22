41d10c2406fa5bfcabdf48337141e9327fa25c44
José Miguel Horcas <horcas@uma.es>
2023-06-22 13:28:31 +0200

    Add captcha to login

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -4,27 +4,35 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from . import auth_bp
 from .models import User, get_user
-
+from utils.recaptcha import is_human
 
 
 @auth_bp.route('/login/', methods=['GET', 'POST'])
 def login():
+    config = {}
+    config['pub_key'] = current_app.config['RECAPTCHA_SITE_KEY']
+
     if current_user.is_authenticated:
         return redirect(url_for('admin.index'))
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        remember_me = True if request.form.get('rememberme', False) == 'on' else False
-
-        user = get_user(email)
-        if user is not None and user.check_password(password):
-            login_user(user, remember=remember_me)
-            next_page = request.args.get('next')
-            if not next_page or url_parse(next_page).netloc != '':
-                next_page = url_for('admin.index')
-            return redirect(next_page)
+        captcha_response = request.form['g-recaptcha-response']
+        if not is_human(captcha_response):
+            flash(f'Invalid captcha.', category='error')
+            return redirect(request.referrer)
         else:
-            flash(f'Invalid email or password.', category='error')
+            email = request.form['email']
+            password = request.form['password']
+            remember_me = True if request.form.get('rememberme', False) == 'on' else False
+
+            user = get_user(email)
+            if user is not None and user.check_password(password):
+                login_user(user, remember=remember_me)
+                next_page = request.args.get('next')
+                if not next_page or url_parse(next_page).netloc != '':
+                    next_page = url_for('admin.index')
+                return redirect(next_page)
+            else:
+                flash(f'Invalid email or password.', category='error')
     # Create default admin
     ENVIRONMENT_ADMIN_USER_EMAIL = os.environ.get("ADMIN_USER_EMAIL")
     ENVIRONMENT_ADMIN_PASS = os.environ.get("ADMIN_PASS")
@@ -35,7 +43,7 @@ def login():
         user.email = ENVIRONMENT_ADMIN_USER_EMAIL
         user.set_password(ENVIRONMENT_ADMIN_PASS)
         user.save()
-    return render_template('auth/login.html')
+    return render_template('auth/login.html', config=config)
 
 
 @auth_bp.route("/signup/", methods=["GET", "POST"])
